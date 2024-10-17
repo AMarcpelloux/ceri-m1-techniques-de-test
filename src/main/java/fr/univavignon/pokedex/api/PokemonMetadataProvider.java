c9c03bb89e9c8de5fc318f63f4fec594b3cbb51a
Marc Pelloux <marc.pelloux@alumni.univ-avignon.fr>
2024-10-17 11:18:29 +0200

    refactor PokemonMetadataProvider

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -3,9 +3,7 @@ package fr.univavignon.pokedex.api;
 public class PokemonMetadataProvider implements IPokemonMetadataProvider{
     static private PokemonMetadataProvider pokemonMetadataProvider;
     static private PokemonMetadata[] pokemonMetadata;
-    static private int id;
     private PokemonMetadataProvider() {
-        id = 0;
         pokemonMetadata =  new PokemonMetadata[151];
     }
     public static PokemonMetadataProvider getProvider(){
@@ -17,14 +15,14 @@ public class PokemonMetadataProvider implements IPokemonMetadataProvider{
 
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-        if((id>0&&id<=150) && pokemonMetadata[index] != null)
+        if((index>=0&&index<=150) && pokemonMetadata[index] != null)
             return pokemonMetadata[index];
         else throw new PokedexException("Pokemon dont exist");
     }
     static public void addPokemonMetadata(PokemonMetadata PM){
-        if(id<=150){
-            pokemonMetadata[id]=PM;
-            id++;
+        if(PM.getIndex()<=150){
+            pokemonMetadata[PM.getIndex()]=PM;
+
         }
     }
 }
