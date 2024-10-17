059895571da3eaa19e55afdc4a52590783ce97bb
Marc Pelloux <marc.pelloux@alumni.univ-avignon.fr>
2024-10-17 09:53:31 +0200

    test IPokemonFactoryTest et IPokemonTrainerFactoryTest

--- src/Test/Java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/Test/Java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,7 +1,29 @@
 package fr.univavignon.pokedex.api;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 public class IPokemonFactoryTest {
+    @Mock
+    PokemonFactory pokemonFactory;
 
+    @Test
+    void ReturnPokemonClassWhenPokemonFactoryCreatePokemon(){
+        when(pokemonFactory.createPokemon(1,1,1,1,1)).thenReturn(new Pokemon(1,"Jean",1,1,1,1,1,1,1,1));
+        assertEquals(Pokemon.class,pokemonFactory.createPokemon(1,1,1,1,1).getClass());
+    }
+    @Test
+    void ReturnValidPokemonWhenPokemonFactoryCreatePokemon(){
+        when(pokemonFactory.createPokemon(1,1,1,1,1)).thenReturn(new Pokemon(1,"Jean",1,1,1,1,1,1,1,1));
+        Pokemon P = new Pokemon(1,"Jean",1,1,1,1,1,1,1,1);
+
+        assertEquals(P.getIndex(),pokemonFactory.createPokemon(1,1,1,1,1).getIndex());
+        assertEquals(P.getName(),pokemonFactory.createPokemon(1,1,1,1,1).getName());
+    }
 }
--- src/Test/Java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/Test/Java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,5 +1,22 @@
 package fr.univavignon.pokedex.api;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class IPokemonTrainerFactoryTest {
-
+    @Mock
+    IPokemonTrainerFactory pokemonTrainerFactory;
+    IPokedexFactory pokedexFactory;
+    IPokedex pokedex;
+    @Test
+    void WhenCreateTrainerReturnTrainerClass(){
+        when(pokemonTrainerFactory.createTrainer("Jean",Team.MYSTIC,pokedexFactory)).thenReturn(new PokemonTrainer("Jean",Team.MYSTIC,pokedex));
+        assertEquals(PokemonTrainer.class,pokemonTrainerFactory.createTrainer("Jean",Team.MYSTIC,pokedexFactory).getClass());
+    }
 }
